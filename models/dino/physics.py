125c296643d5aa12817ce9db55a63f728c7b90ca
wangyunqi <wangyunqi@MacBook-Pro-3.local>
2025-10-29 12:17:51 +0800

    update

---
+++ models/dino/physics.py
@@ -0,0 +1,22 @@
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class PhysHeadWithOW(nn.Module):
+
+class FixedSobel(nn.Module):
+
+class FixedLaplacian(nn.Module):
+
+
+class StructureTensor(nn.Module):
+
+class LearnablePhysHead(nn.Module):
+
+class PhysVisualFusion(nn.Module):
+
+class QueryInitializer(nn.Module):
+
+
+
